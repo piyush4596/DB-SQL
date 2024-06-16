10ecd26074d17b20566c4b5149713e07b58c4dad
piyush4596 <piyushprajapati3008@gmail.com>
2024-06-16 13:26:47 -0400

    add home page

--- index.js
+++ index.js
@@ -1,5 +1,11 @@
 const { faker } = require("@faker-js/faker");
 const mysql = require("mysql2");
+const express = require("express");
+const app = express();
+const path = require("path");
+
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "/views"));
 
 const connection = mysql.createConnection({
   host: "localhost",
@@ -8,21 +14,50 @@ const connection = mysql.createConnection({
   password: "PgUpaction#1",
 });
 
-try {
-  connection.query("SHOW TABLES", (err, result) => {
-    if (err) throw err;
-    console.log(result);
-  });
-} catch (err) {
-  console.log(err);
-}
-connection.end();
-
 let getRandomUser = () => {
-  return {
-    id: faker.string.uuid(),
-    username: faker.internet.userName(),
-    email: faker.internet.email(),
-    password: faker.internet.password(),
-  };
+  return [
+    faker.string.uuid(),
+    faker.internet.userName(),
+    faker.internet.email(),
+    faker.internet.password(),
+  ];
 };
+
+// let q = "INSERT INTO user (id, username, email, password) VALUES ?";
+
+// let data = [];
+// for (let i = 0; i <= 100; i++) {
+//   data.push(getRandomUser());
+// }
+// let users = [
+//   ["123b", "123_newb", "new@gmail.comb", "abcb"],
+//   ["123c", "123_newc", "new@gmail.comc", "abcc"],
+// ];
+
+// try {
+//   connection.query(q, [data], (err, result) => {
+//     if (err) throw err;
+//     console.log(result);
+//   });
+// } catch (err) {
+//   console.log(err);
+// }
+// connection.end();
+
+app.get("/", (req, res) => {
+  let q = "SELECT count(*) FROM user";
+  try {
+    connection.query(q, (err, result) => {
+      if (err) throw err;
+      let count = result[0]["count(*)"];
+      res.render("home.ejs", { count });
+    });
+  } catch (err) {
+    console.log(err);
+    res.send("some error DB");
+  }
+});
+
+app.listen("8080", () => {
+  console.log("server is listening on 8080");
+});
